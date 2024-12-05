605f63039092cccaa2135b973a7fe72f5ab7a277
Mopacak <diegopavlovic051@gmail.com>
2024-12-05 14:09:57 +0100

    Ispit rijesen

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # test-MO
+link gita:
+https://github.com/Mopacak/test-MO.git
+
+
+Upit: 
+SELECT z.ime_prezime, SUM(e.broj_sati) AS ukupno_sati
+FROM evidencija e
+JOIN zaposlenik z ON e.zaposlenik = z.id
+WHERE z.id = 1  
+GROUP BY z.ime_prezime;
+
+
+
---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+
+   
+    <q-card class="q-mt-md" style="width: 300px; text-align: center">
+      <q-card-section>
+        <div class="text-h6">Dobrodosli u quasar</div>
+        <p>komponenta qcard dodana.</p>
+      </q-card-section>
+
+      <q-btn label="klikni" color="primary" class="q-mt-md" />
+    </q-card>
+  </q-page>
+</template>
+
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
